da0b60dc8080e5296be4a5f8e3653ad3c0e9b3c4
Cassandre Houze <cassandredelaulnoit@gmail.com>
2019-11-26 15:56:16 +0100

    exo2 finished

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2019_11_26_110228) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.string "details"
+    t.text "details"
     t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
